9f99ac46a8c352b65ea0143f3a34afb3341f9ae0
vixidurr2402 <chien27122402@gmail.com>
2024-05-31 02:18:51 +0700

    vanchien

---
+++ BTL_DeTai1/src/btl_detai1/BTL_DeTai1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package btl_detai1;
+
+/**
+ *
+ * @author Admin
+ */
+public class BTL_DeTai1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
